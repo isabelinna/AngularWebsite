4e58e56c34aff0d8c8e838d6e9dd98c1c24d8e0f
isabelinna <isabelinna.bel@gmail.com>
2022-04-21 20:02:56 -0500

    added component

--- Beat-Dancer/src/app/app-routing.module.ts
+++ Beat-Dancer/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//Añadir rutas
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+//Declarar rutas (Al entrar a la página nos llevara a home)
+const routes: Routes = [{ path: '', redirectTo: 'home', pathMatch: 'full'},
+{ path: 'home', component: HomeComponent },
+{ path: 'about', component: AboutComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
